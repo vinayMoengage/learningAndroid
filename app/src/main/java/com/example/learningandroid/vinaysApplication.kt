3208d8f8e0c3b9f81aac697f3df9cba7e71adb19
vinayMoengage <vinay.kumar@moengage.com>
2022-02-16 18:15:10 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/learningandroid/vinaysApplication.kt
@@ -0,0 +1,20 @@
+package com.example.learningandroid
+import android.app.Application
+import android.util.Log
+import com.moengage.core.LogLevel
+import com.moengage.core.MoEngage
+import com.moengage.core.config.LogConfig
+
+class vinaysApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.e("Testing", "Inside Application class method onCreate()")
+
+        val moEngage = MoEngage.Builder(this, "T2I79ZWOB4MPWPYHF2CPYQ36")
+            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
+            .build()
+        MoEngage.initialise(moEngage)
+    }
+}
